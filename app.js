a12e031006e4e71e32c051fe48988f03aed3094e
Emre Goktug Kayis <emregokktug@gmail.com>
2026-01-28 13:20:03 +0300

    update

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("index");
+//Template Engine
+app.set('view engine', 'ejs');
+
+//MIDDLEWARE
+app.use(express.static('public'));
+
+//ROUTES
+app.get('/', (req, res) => {
+  res.status(200).render('index', {
+    page_name: 'index',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.status(200).render('about', {
+    page_name: 'about',
+  });
 });
 
 const port = 3000;
